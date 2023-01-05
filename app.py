97127c52c225d81d3bdab88172bc990f3ccd4cdd
Matt <dain6735@gmail.com>
2023-01-04 21:44:29 -0600

    testing luckynumber + index

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask
 app = Flask(__name__)
 
+app = Flask(  # Create a flask app
+	__name__,
+	template_folder='templates',  # Name of html file folder
+	static_folder='static'  # Name of directory for static files
+)
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+  return render_template('index.html')
+
+@app.route('/luckynumber')
+def luckNum():
+  luckyNumbers = ""
+  for i in range(7):
+    rand = random.randint(0,100)
+    luckyNumbers += str(rand)
+    luckyNumbers += " "
+  return render_template('luckyNumber.html', luckyNumbers = luckyNumbers)
